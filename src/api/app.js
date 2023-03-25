56cf26f2c98361fa49686c4b0e29ba5693cf14c2
dhins <dhins@moonshine.mu>
2023-03-24 21:59:30 -0400

    "url changed for ipServer"

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'http://167.99.153.95/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost)
