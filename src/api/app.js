e0f78a8fd08cf7119ec54ab46755e172b1c397d3
dhins <dhins@moonshine.mu>
2023-04-03 16:11:34 -0400

    "url / post added"

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-app-server-nu.vercel.app//posts';
+const url = 'https://memories-app-server-nu.vercel.app/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost)
