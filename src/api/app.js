719ebbea31ee631e0d867729b5fcc436cc3d2cc6
dhins <dhins@moonshine.mu>
2023-04-03 00:40:36 -0400

    "domain server added"

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://167.99.153.95/posts';
+const url = 'http://memories-server.xyz/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost)
