422727a07f256169f250a5b1f64bb54986b8a78f
dhins <dhins@moonshine.mu>
2023-04-03 00:53:51 -0400

    "https added"

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://memories-server.xyz/posts';
+const url = 'https://memories-server.xyz/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost)
