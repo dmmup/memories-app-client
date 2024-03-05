633d12485bb05b8fa740a3b7cae7c0e8cd805825
dhins <dhins@moonshine.mu>
2024-03-05 12:58:24 -0500

    meta tag

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose  } from 'redux'
 import thunk from 'redux-thunk'
-
+import {Helmet} from "react-helmet";
 import reducers from './reducers'
+
+
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
+  
   <Provider store={store}>
+    <Helmet>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+                
+    </Helmet>
     <App />
   </Provider>
 );
