eeb132f337cc1aa02c0194deac0a59f894b13cc9
dhins <dhins@moonshine.mu>
2024-03-05 11:46:19 -0500

    ip

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-app-server-1.vercel.app/posts';
+const url = 'http://192.168.0.160:5000/posts';
 
 
 export const fetchPosts = () => axios.get(url);
